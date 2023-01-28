a75624d8aed87971039bf97c8ae47d491e1980e0
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-28 23:50:38 +0100

    feat(header): added useeffect to update time every second

--- client/app/Header.tsx
+++ client/app/Header.tsx
@@ -1,9 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { CiGlobe } from "react-icons/ci";
 
 export const Header = () => {
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date().toLocaleString());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <header className="fixed z-10 top-0 w-screen h-7 flex items-center justify-end px-6 text-xs bg-gradient-to-r from-emerald-800 via-gray-700 to-emerald-900 shadow-sm">
-      <div className="text-emerald-100"> {new Date().toLocaleString()} </div>
+      <div className="text-emerald-100"> {date} </div>
       <CiGlobe className="text-emerald-100 text-base ml-4" />
     </header>
   );
