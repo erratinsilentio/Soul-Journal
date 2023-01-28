ddafac59f8a656f11518507d850276be94b1d568
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-28 11:20:02 +0100

    feat(navbar): added Links to subpages

--- client/app/Navbar.tsx
+++ client/app/Navbar.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import { CiStickyNote, CiSettings, CiRead, CiCalendar } from "react-icons/ci";
 
 export const Navbar = () => {
   return (
     <nav className="fixed z-0 top-0 left-0 flex flex-col py-16 items-center min-h-screen w-20 bg-gradient-to-b from-gray-800 via-gray-800 to-gray-900">
-      <CiStickyNote className="text-emerald-700 text-3xl duration-200 hover:text-4xl" />
-      <CiRead className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
-      <CiCalendar className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
-      <CiSettings className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
+      <Link href="/">
+        <CiStickyNote className="text-emerald-700 text-3xl duration-200 hover:text-4xl" />
+      </Link>
+      <Link href="/goals">
+        <CiRead className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
+      </Link>
+      <Link href="/archive">
+        <CiCalendar className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
+      </Link>
+      <Link href="/settings">
+        <CiSettings className="text-emerald-700 text-3xl duration-200 hover:text-4xl mt-14" />
+      </Link>
     </nav>
   );
 };
