bd3e907aa8a57335e5510239fc2509e5afd1db01
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-01 00:42:01 +0100

    feat(dark mode): added dark mode slice to redux store

--- client/store/store.ts
+++ client/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "./menuSlice";
+import themeReducer from "./themeSlice";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    theme: themeReducer,
   },
 });
 
