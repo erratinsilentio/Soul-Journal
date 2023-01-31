9e50c6cb650099c4155f20c39705f35b50b6b2a7
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-31 18:10:57 +0100

    feat(navbar): extracted icons into seperate component, added extra text and modified styling

---
+++ client/components/layout/Icon.tsx
@@ -0,0 +1,31 @@
+import { ReactElement } from "react";
+import { IconType } from "react-icons/lib";
+import { SelectedPath } from "./Navbar";
+
+export const NavbarIcon = ({
+  icon,
+  text,
+  selected,
+}: {
+  icon: ReactElement;
+  text: SelectedPath;
+  selected: SelectedPath;
+}) => {
+  return (
+    <div
+      className={`group relative my-7 text-3xl text-emerald-700 duration-200 hover:text-emerald-500 ${
+        selected === text && "text-emerald-500"
+      }`}
+    >
+      {icon}
+      <span
+        className="absolute left-14 m-2 w-auto min-w-max origin-left scale-0 rounded-md
+    bg-gray-900 p-2 
+    text-xs font-bold 
+    text-white shadow-md transition-all duration-100 group-hover:scale-100"
+      >
+        {text}
+      </span>
+    </div>
+  );
+};
--- client/components/layout/Navbar.tsx
+++ client/components/layout/Navbar.tsx
@@ -5,12 +5,13 @@ import Link from "next/link";
 import { useState } from "react";
 import { CiStickyNote, CiSettings, CiRead, CiCalendar } from "react-icons/ci";
 import { useSelector } from "react-redux";
+import { NavbarIcon } from "./Icon";
 
-type SelectedPath = "home" | "goals" | "archive" | "settings";
+export type SelectedPath = "Home" | "Goals" | "Archive" | "Settings";
 
 export const Navbar = () => {
   const menuState = useSelector((state: RootState) => state.menu);
-  const [selected, setSelected] = useState<SelectedPath>("home");
+  const [selected, setSelected] = useState<SelectedPath>("Home");
 
   return (
     <nav
@@ -18,33 +19,17 @@ export const Navbar = () => {
         menuState ? "left-0" : "-left-20"
       } fixed top-0 z-10 flex min-h-screen w-16 flex-col items-center bg-gradient-to-b from-gray-800 via-gray-800 to-gray-900 py-16 duration-200 sm:left-0 sm:w-20`}
     >
-      <Link href="/" onClick={() => setSelected("home")}>
-        <CiStickyNote
-          className={`text-3xl text-emerald-700 duration-200 hover:text-emerald-500 ${
-            selected === "home" && "text-emerald-500"
-          }`}
-        />
+      <Link href="/" onClick={() => setSelected("Home")}>
+        <NavbarIcon icon={<CiStickyNote />} selected={selected} text="Home" />
       </Link>
-      <Link href="/goals" onClick={() => setSelected("goals")}>
-        <CiRead
-          className={`mt-14 text-3xl text-emerald-700 duration-200 hover:text-emerald-500 ${
-            selected === "goals" && "text-emerald-500"
-          }`}
-        />
+      <Link href="/goals" onClick={() => setSelected("Goals")}>
+        <NavbarIcon icon={<CiRead />} selected={selected} text="Goals" />
       </Link>
-      <Link href="/archive" onClick={() => setSelected("archive")}>
-        <CiCalendar
-          className={`mt-14 text-3xl text-emerald-700 duration-200 hover:text-emerald-500 ${
-            selected === "archive" && "text-emerald-500"
-          }`}
-        />
+      <Link href="/archive" onClick={() => setSelected("Archive")}>
+        <NavbarIcon icon={<CiCalendar />} selected={selected} text="Archive" />
       </Link>
-      <Link href="/settings" onClick={() => setSelected("settings")}>
-        <CiSettings
-          className={`mt-14 text-3xl text-emerald-700 duration-200 hover:text-emerald-500 ${
-            selected === "settings" && "text-emerald-500"
-          }`}
-        />
+      <Link href="/settings" onClick={() => setSelected("Settings")}>
+        <NavbarIcon icon={<CiSettings />} selected={selected} text="Settings" />
       </Link>
     </nav>
   );
