3aa46c6b1b5e33dda73b836dff1699d5591c0d8a
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-02 01:35:20 +0100

    started drafting basic sign up form to test auth functions

--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import { supabase } from "@/supabase";
+import { useState } from "react";
+
 const Settings = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rMsg, setRMsg] = useState("");
+  const [user, setUser] = useState({});
+
+  const register = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      setRMsg(error.message);
+    } else {
+      setUser(data);
+    }
+  };
+
   return (
-    <main className="z-0 min-h-screen min-w-full bg-gradient-to-b from-stone-800 via-stone-800 to-stone-900">
-      settings
+    <main className="z-0 flex min-h-screen min-w-full items-center justify-center bg-gradient-to-b from-stone-800 via-stone-800 to-stone-900">
+      <form>
+        <label>email</label>
+        <input
+          name="email"
+          id="email"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>username</label>
+        <input
+          name="username"
+          id="username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          name="password"
+          id="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
     </main>
   );
 };
