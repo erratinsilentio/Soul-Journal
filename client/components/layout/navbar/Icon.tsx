ca5b12a1aadb4586b8d18f31681fbad8e249c3b1
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-02 08:40:23 +0100

    feat(navbar): fixed icon styling, so now they lit up when selected

--- client/components/layout/navbar/Icon.tsx
+++ client/components/layout/navbar/Icon.tsx
@@ -1,22 +1,17 @@
+"use client";
+
 import { ReactElement } from "react";
-import { IconType } from "react-icons/lib";
 import { SelectedPath } from "./Navbar";
 
 export const NavbarIcon = ({
   icon,
   text,
-  selected,
 }: {
   icon: ReactElement;
   text: SelectedPath;
-  selected: SelectedPath;
 }) => {
   return (
-    <div
-      className={`group relative my-7 text-3xl text-emerald-700 duration-200 hover:scale-110 hover:text-emerald-500 ${
-        selected === text && "text-emerald-500"
-      }`}
-    >
+    <div className="group relative my-7 text-3xl text-emerald-700 duration-200 hover:scale-110 hover:text-emerald-500">
       {icon}
       <span
         className="absolute -top-2 left-14 m-2 w-auto min-w-max origin-left scale-0 rounded-md
--- client/components/layout/navbar/Navbar.tsx
+++ client/components/layout/navbar/Navbar.tsx
@@ -20,16 +20,44 @@ export const Navbar = () => {
       } fixed top-0 z-10 flex min-h-screen w-16 flex-col items-center bg-gradient-to-b from-zinc-800 via-zinc-800 to-zinc-900 py-16 shadow-2xl shadow-black duration-200 sm:left-0 sm:w-20`}
     >
       <Link href="/" onClick={() => setSelected("Home")}>
-        <NavbarIcon icon={<CiStickyNote />} selected={selected} text="Home" />
+        <NavbarIcon
+          icon={
+            <CiStickyNote
+              className={`${selected === "Home" && "text-emerald-500"}`}
+            />
+          }
+          text="Home"
+        />
       </Link>
       <Link href="/goals" onClick={() => setSelected("Goals")}>
-        <NavbarIcon icon={<CiRead />} selected={selected} text="Goals" />
+        <NavbarIcon
+          icon={
+            <CiRead
+              className={`${selected === "Goals" && "text-emerald-500"}`}
+            />
+          }
+          text="Goals"
+        />
       </Link>
       <Link href="/archive" onClick={() => setSelected("Archive")}>
-        <NavbarIcon icon={<CiCalendar />} selected={selected} text="Archive" />
+        <NavbarIcon
+          icon={
+            <CiCalendar
+              className={`${selected === "Archive" && "text-emerald-500"}`}
+            />
+          }
+          text="Archive"
+        />
       </Link>
       <Link href="/settings" onClick={() => setSelected("Settings")}>
-        <NavbarIcon icon={<CiSettings />} selected={selected} text="Settings" />
+        <NavbarIcon
+          icon={
+            <CiSettings
+              className={`${selected === "Settings" && "text-emerald-500"}`}
+            />
+          }
+          text="Settings"
+        />
       </Link>
     </nav>
   );
