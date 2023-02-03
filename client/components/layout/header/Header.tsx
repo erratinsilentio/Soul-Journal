1289d9d127de72c886927b95e62920800494e852
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-03 19:56:53 +0100

    feat(header): adding logo

--- client/components/layout/header/Header.tsx
+++ client/components/layout/header/Header.tsx
@@ -6,6 +6,7 @@ export const Header = () => {
   return (
     <header className="fixed left-0 top-0 z-40 flex h-7 w-screen items-center justify-end bg-gradient-to-r from-emerald-800 via-slate-700 to-emerald-900 px-6 text-xs shadow-emerald-700">
       <Hamburger />
+      <div className="fixed left-3">Soul Journal</div>
       <Clock />
       <ThemeButton />
     </header>
