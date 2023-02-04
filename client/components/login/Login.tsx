baef6d2752ae28c8f4f1df40ed42993e8b8ceb6b
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-04 08:34:18 +0100

    feat(login page): extracted login header to seperate component

--- client/components/login/Login.tsx
+++ client/components/login/Login.tsx
@@ -1,14 +1,10 @@
 import { LoginForm } from "./LoginForm";
+import { LoginHeader } from "./LoginHeader";
 
 export const LoginPage = () => {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-start justify-start bg-gradient-to-b from-stone-800 via-stone-800 to-stone-900 p-32">
-      <span className="mb-10 text-3xl font-semibold text-zinc-200">
-        🌙 Welcome to Soul Journal
-      </span>
-      <span className="mb-10 text-2xl font-thin text-zinc-400">
-        Type your email to receive one time login link
-      </span>
+      <LoginHeader />
       <LoginForm />
     </main>
   );
---
+++ client/components/login/LoginHeader.tsx
@@ -0,0 +1,13 @@
+export const LoginHeader = () => {
+  return (
+    <>
+      342kl
+      <span className="mb-10 text-3xl font-semibold text-zinc-200">
+        🌙 Welcome to Soul Journal
+      </span>
+      <span className="mb-10 text-2xl font-thin text-zinc-400">
+        Type your email to receive one time login link
+      </span>
+    </>
+  );
+};
