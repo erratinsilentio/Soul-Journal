580f4a730ad7cec75d69a23b50265b8225488c6b
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-17 11:49:20 +0100

    fix(home page): removed usestate and useffect

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,30 +1,21 @@
 "use client";
 import { DailyNoteForm } from "@/components/home/DailyNoteForm";
-import { RootState } from "@/store/store";
+import { RootState, useAppSelector } from "@/store/store";
 import { checkIfDailyNoteExists } from "@/utils/noteActions";
 import { ProtectedWrapper } from "@/utils/ProtectedWrapper";
 import { useQuery } from "@tanstack/react-query";
-import { useSelector } from "react-redux";
 import { Error } from "@/components/error/Error";
 import { Loading } from "@/components/loading/Loading";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const isDark = useSelector((state: RootState) => state.theme);
-  const user = useSelector((state: RootState) => state.user.user);
-
-  const [userID, setUserID] = useState(null);
-
-  useEffect(() => {
-    setUserID((userID) => user?.id);
-    console.log("id", userID);
-  }, [user]);
+  const isDark = useAppSelector((state: RootState) => state.theme);
+  const user = useAppSelector((state: RootState) => state.user.user);
 
   const {
     data: dailyNoteData,
     isLoading,
     error,
-  } = useQuery(["daily", userID], () => checkIfDailyNoteExists(userID));
+  } = useQuery(["daily", user?.id], () => checkIfDailyNoteExists(user?.id));
 
   if (isLoading) return <Loading />;
   if (error) return <Error />;
@@ -36,7 +27,7 @@ export default function Home() {
           isDark ? "light" : "dark"
         } z-0 min-h-screen min-w-full p-5 sm:p-10`}
       >
-        <DailyNoteForm dailyNote={dailyNoteData} userID={userID} />
+        <DailyNoteForm dailyNote={dailyNoteData} userID={user?.id} />
       </main>
     </ProtectedWrapper>
   );
