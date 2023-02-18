d49b13c19d71893fec5b9ab2bfee34a7fdd92dce
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-18 22:41:37 +0100

    feat(error component): added icon to error component

--- client/components/error/Error.tsx
+++ client/components/error/Error.tsx
@@ -1,7 +1,8 @@
+import { RiErrorWarningLine } from "react-icons/ri";
 export const Error = () => {
   return (
-    <main className="items z-0 flex min-h-screen min-w-full justify-center p-5 sm:p-10">
-      error
+    <main className="z-0 flex min-h-screen min-w-full items-center justify-center p-5 sm:p-10">
+      <RiErrorWarningLine />
     </main>
   );
 };
