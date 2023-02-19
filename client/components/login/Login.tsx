1b49230d460eef6b52b7ad06fb64afcabd982e35
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-20 00:36:05 +0100

    added missing animation on login page

--- client/components/login/Login.tsx
+++ client/components/login/Login.tsx
@@ -1,13 +1,20 @@
+"use client";
 import { GoogleButton } from "./GoogleButton";
 import { LoginForm } from "./LoginForm";
 import { LoginHeader } from "./LoginHeader";
+import { motion } from "framer-motion";
 
 export const LoginPage = () => {
   return (
-    <main className="min-w-screen flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-stone-800 via-stone-800 to-stone-900 p-8 sm:items-start sm:p-16 md:p-32">
+    <motion.main
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5 }}
+      className="min-w-screen flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-stone-800 via-stone-800 to-stone-900 p-8 sm:items-start sm:p-16 md:p-32"
+    >
       <LoginHeader />
       <LoginForm />
       <GoogleButton />
-    </main>
+    </motion.main>
   );
 };
